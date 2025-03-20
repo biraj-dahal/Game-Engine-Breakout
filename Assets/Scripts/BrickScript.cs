8d9233535ee7d60f81a597cdfac6cd5a5fb5e102
biraj-dahal <dahalbiraj10@gmail.com>
2025-03-19 23:00:03 -0400

    brick dettects collison, breaks, and reduces child count checks

--- Assets/Scripts/BrickScript.cs
+++ Assets/Scripts/BrickScript.cs
@@ -5,16 +5,45 @@ public class BrickScript : MonoBehaviour
 
 {
     public int brickValue;
-    // private void OnCollisionEnter(Collision collision)
-    // {
-    //     if (collision.gameObject.name == "Ball")
-    //     {
-    //         //Play sound
-    //         AudioSource audio = GetComponent<AudioSource>();
-    //         audio.Play();
+    public bool isStrong;
+    public int hitsLeft;
+    public int STRONG_HITS = 2;
 
-    //         Destroy(this.gameObject);
-    //     }
-    // }
-    
+    public int ScoreVal;
+
+    private void Start()
+    {
+        if (isStrong)
+        {
+            hitsLeft = STRONG_HITS;
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+{
+    if (collision.gameObject.tag == "Ball")
+    {
+        if (isStrong)
+        {
+            if (hitsLeft > 0)
+            {
+                hitsLeft--;
+            }
+            else
+            {
+                GameManager.S.IncreaseScore(brickValue);
+                GameManager.S._checkBricks = true; 
+                Destroy(this.gameObject);
+            }
+        }
+        else
+        {
+            GameManager.S.IncreaseScore(brickValue);
+            GameManager.S._checkBricks = true;  
+            Destroy(this.gameObject);
+        }
+    }
+}
+
 }
