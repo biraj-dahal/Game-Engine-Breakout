b4ca40faaa4f822522409b0176598a2f62e2ffb8
biraj-dahal <dahalbiraj10@gmail.com>
2025-03-19 22:59:36 -0400

    shift the work of collision destroy from ball to brick

--- Assets/Scripts/Ballscript.cs
+++ Assets/Scripts/Ballscript.cs
@@ -5,49 +5,14 @@ using UnityEngine.UI;
 
 public class Ballscript : MonoBehaviour
 {
-    public float ballSpeed = 1.0f;
 
-    public Text ScoreVal;
-
-    private int score = 0;
-
-    private SoundManager mySoundManager;  // Fixed type from GameObject to SoundManager
-
-    public Text label;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        // Correctly get the SoundManager component
-        mySoundManager = GameObject.Find("SoundManager").GetComponent<SoundManager>();
-
-        // get the Rigidbody component
-        Rigidbody rb = GetComponent<Rigidbody>();
-
-        // set the direction
-        Vector3 direction = new Vector3(5.0f, 4.0f, 0.0f);
-        direction.Normalize();
-
-        // set the velocity
-        rb.linearVelocity = direction * ballSpeed; 
-        ScoreVal.text = "0";
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnCollisionEnter(Collision collision)
     {
-
-        mySoundManager.PlayGameSound(collision.gameObject.tag); 
-        if (collision.gameObject.tag == "Brick")
+        SoundManager.S.PlayGameSound(collision.gameObject.tag);
+        if (collision.gameObject.tag == "GameOver" && GameManager.S.curState == GameState.Playing)
         {
-            Destroy(collision.gameObject);
-            score += collision.gameObject.GetComponent<BrickScript>().brickValue;
-            ScoreVal.text = score.ToString();
+           GameManager.S.BallLeftPlay();
         }
     
     }
